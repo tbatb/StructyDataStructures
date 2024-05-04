a15a4aa109e4bcdbf248a80ab892cd27632268dc
Tegshbayar Batbayar <tegshbayar.batbayar@outlook.com>
2024-05-04 23:07:08 +0200

    LinkedList Node package/chapter started
    Signed-off-by: Tegshbayar Batbayar <tegshbayar.batbayar@outlook.com>

--- src/LinkedList/LinkedList.java
+++ src/LinkedList/LinkedList.java
@@ -1,4 +1,39 @@
 package LinkedList;
 
+
 public class LinkedList {
+
+    public static void main(String[] args){
+        run();
+    }
+
+    public LinkedList(){
+        Node node = new Node(5);
+
+    }
+
+    public static void run(){
+        Node<String> a = new Node<>("a");
+        Node<String> b = new Node<>("b");
+        Node<String> c = new Node<>("c");
+
+        a.next = b;
+        b.next = c;
+
+        Node<Integer> node1 = new Node<>(42);
+
+        System.out.println(a.next.value);
+        System.out.println(node1.value);
+    }
+
+
+    private static class Node<T> {
+        T value;
+        Node next;
+
+        private Node(T value){
+            this.value = value;
+            this.next = null;
+        }
+    }
 }
