4733a154c75e070d5434330843bac1dbf33c682f
Tegshbayar Batbayar <tegshbayar.batbayar@outlook.com>
2024-05-04 11:31:03 +0200

    Introd

--- src/Introduction.java
+++ src/Introduction.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class Introduction {
+public class Introduction {
+    public static double maxValue(double[] numbers) {
+        // todo
+        int i = 0;
+        double  max = numbers[i];
+        // use negative infinity to cover the edge case for negative normal numbers.
+        double inf = Double.NEGATIVE_INFINITY;
+
+        while (i < numbers.length){
+            if (numbers[i] > max){
+                max = numbers[i];
+            }
+            i++;
+        }
+
+
+
+        return max;
+    }
+
+    public static boolean isPrime(int number) {
+        // todo
+        if (number <= 1){
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(number); i++){
+            if (number % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public static void run() {
+        // this function behaves as `main()` for the 'run' command
+        // you may sandbox in this function, but should not remove it
+    }
+
 }
