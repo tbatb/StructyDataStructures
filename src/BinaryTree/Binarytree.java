5936bd5d417a1c061d10191cd95f15abb016663f
Tegshbayar Batbayar <tegshbayar.batbayar@outlook.com>
2024-05-10 12:02:04 +0200

    added trees
    Signed-off-by: Tegshbayar Batbayar <tegshbayar.batbayar@outlook.com>

--- src/BinaryTree/Binarytree.java
+++ src/BinaryTree/Binarytree.java
@@ -43,44 +43,83 @@ public class Binarytree {
 
     /**
      * Depth First Traversal Problem: Our method, depthFirstValues, takes in the root of a binary tree.
-     * It will add all values onto a seperate list.
+     * It will add all values into a separate list. NOT WORKING ATM
      *
      * @param root
      * @return The method should return a list containing all values of the tree in depth-first order.
      * Time O(n), Space O(n)
      */
-    /*
+
     public static List<String> depthFirstValues(TreeNode<String> root){
+        //TODO:using java frameworks is not allowed in the EP2 Test1, so still need to ADD own STACK, QUEUE DS.
         ListStack stack = new ListStack();
         stack.push(root.toString());
         List<String> values = new ArrayList<>();
         while (!stack.isEmpty()) {
-            TreeNode current = stack.pop();
-            values.add(current.value)
+            //TreeNode current = stack.pop();
+            //values.add(current.value)
             return null;
         }
 
         return values;
     }
 
+
+    /**
+     * This methods takes in the root of a binary tree and traverses the tree using depth first order.
+     *
+     * @param root
+     * @return This method should return a List containing all values of the tree in depth-first order.
      */
     public static List<String> depthFristValueswithStack(TreeNode<String> root){
+        if (root == null) {
+            return List.of();
+        }
 
+        List<String> values = new ArrayList<>();
         Stack<TreeNode<String>> stack = new Stack<>();
         stack.push(root);
-        List<String> values = new ArrayList<>();
-        while (!stack.isEmpty()) {
+        while (!stack.empty()) {
             TreeNode<String> current = stack.pop();
             values.add(current.val);
-            if (current.left != null) {
-                stack.push(current.left);
-            }
             if (current.right != null) {
                 stack.push(current.right);
+            }
+            if (current.left != null) {
+                stack.push(current.left);
             }
         }
         return values;
     }
+
+    /**
+     * Base case returns an empty List. root.left, here we have to think what the call should return, the same
+     * for root.right. Root.left should return a list of nodes in depth first order [b, d, e], root.right [c,f].
+     * Next we must create a List, which also contains the root - root.val.
+     *
+     * @param root
+     * @return A list of all nodes in the binary tree using the depth first order traversal algorithm.
+     */
+    public static List<String> RecursiveDepthFristValueswithStack(TreeNode<String> root){
+        if (root == null) {
+            return List.of();
+        }
+
+        List<String> leftVals = RecursiveDepthFristValueswithStack(root.left);
+        List<String> rightVals = RecursiveDepthFristValueswithStack(root.right);
+        List<String> result = new ArrayList<>();
+        result.add(root.val);
+        result.addAll(leftVals);
+        result.addAll(rightVals);
+
+        return result;
+    }
+
+    //             a!
+    //           /   \
+    //          b     c
+    //         / \     \
+    //        d   e     f
 
 
 }
