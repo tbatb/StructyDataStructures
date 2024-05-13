e474a07e669bebea0fdce5bc42dd68b457c6822e
Tegshbayar Batbayar <tegshbayar.batbayar@outlook.com>
2024-05-13 21:47:47 +0200

    more tree operations
    Signed-off-by: Tegshbayar Batbayar <tegshbayar.batbayar@outlook.com>

--- src/Introduction/StringOperations.java
+++ src/Introduction/StringOperations.java
@@ -2,5 +2,41 @@ package Introduction;
 
 public class StringOperations {
 
+    private int resultStart;
+    private int resultlength;
+
+    /**
+     * Find the longest palindrome in a subtring.
+     *
+     * @param s
+     * @return Longest palindrome
+     */
+    public String longestPalindrom(String s){
+        int strlength = s.length();
+        if (strlength < 2) {
+            return s;
+        }
+        for (int start = 0; start < strlength - 1; start++) {
+            expandRange(s, start, start);
+            expandRange(s, start, start + 1);
+        }
+        return s.substring(resultStart, resultStart + resultlength);
+
+    }
+
+    /**
+     * Helper method for my longestpalindrome method.
+     *
+     * @param str
+     * @param begin
+     * @param end
+     */
+    private void expandRange(String str, int begin, int end){
+        while (begin >= 0 && end < str.length() && str.charAt(begin) == str.charAt(end)) {
+            begin--;
+            end++;
+        }
+    }
+
 
 }
---
+++ src/Introduction/Test.java
@@ -0,0 +1,7 @@
+package Introduction;
+
+public class Test {
+    public static void main(String[] args){
+
+    }
+}
